826af998adffc531ff0f9756e962816cdd455945
Alessandro9827 <ale.moramarco27@gmail.com>
2024-03-11 17:51:25 +0100

    implemented searchbar

--- src/components/saerch/SearchBar.vue
+++ src/components/saerch/SearchBar.vue
@@ -1,7 +1,8 @@
 <template lang="">
     <section class="container">
         <div class="input-group mt-3">
-        <input type="text" class="form-control rounded-pill" placeholder="Cerca destinazioni" aria-label="Cerca destinazioni" aria-describedby="basic-addon2">
+        <input type="text" class="form-control rounded-pill" placeholder="Cerca destinazioni" aria-label="Cerca destinazioni" aria-describedby="basic-addon2"
+        v-model="queryString" @keyup.enter="getApartments(queryString)" >
         <div class="input-group-append">
             <button class="btn btn-danger m-1 rounded-pill" type="button">
                 Cerca
@@ -13,9 +14,39 @@
 </template>
 
 <script>
-    export default {
-        name: 'SearchBar',
-    }
+import axios from 'axios';
+import SingleApartment from '@/pages/SingleApartment.vue';
+
+export default {
+    name: 'SearchBar',
+    data(){
+        return{
+            apartments: [],
+            queryString: '',
+        }
+    },
+    methods:{
+        getPosts(address){
+            axios.get('http://127.0.0.1:8000', {
+                params: {
+                    address: address,
+                }
+            })
+            .then((apartments) => {
+                console.log(apartments);
+                //this.posts = response.data.results;
+
+            })
+            .catch(function (error) {
+                console.warn(error);
+                //this.$router.push({ name: 'not-found' })
+            })
+        }
+    },
+    components:{
+        SingleApartment
+    },
+}
 </script>
 
 <style lang="scss" scoped>
