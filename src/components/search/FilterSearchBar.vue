bceffbe10fcca20d199b756b3402132cd2136e1c
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-03-14 11:29:41 +0100

    aggiunta modale in filtersearchbar

--- src/components/search/FilterSearchBar.vue
+++ src/components/search/FilterSearchBar.vue
@@ -1,5 +1,28 @@
 <template lang="">
-    <div>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-12'>
+                <div class="modal" tabindex="-1">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title">Lista Filtri</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+        </div>
         
     </div>
 </template>
