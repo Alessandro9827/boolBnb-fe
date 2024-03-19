68a633ab8f5ef4a0443541aabd88533d3f2f21c7
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-03-19 18:48:40 +0100

    creato nuovo componente footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,77 @@
+<template >
+    <div>
+        <footer class="footer">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4">
+                  <h5>Airbnb</h5>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Scopri Airbnb</a></li>
+                    <li><a href="#">Opportunità di lavoro</a></li>
+                    <li><a href="#">Newsroom</a></li>
+                    <li><a href="#">Airbnb Plus</a></li>
+                  </ul>
+                </div>
+                <div class="col-md-4">
+                  <h5>Community</h5>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Diversità e appartenenza</a></li>
+                    <li><a href="#">Accessibilità</a></li>
+                    <li><a href="#">Airbnb Associates</a></li>
+                    <li><a href="#">Alloggi per l'emergenza</a></li>
+                  </ul>
+                </div>
+                <div class="col-md-4">
+                  <h5>Ospitare</h5>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Perché affittare</a></li>
+                    <li><a href="#">Ospita responsabilmente</a></li>
+                    <li><a href="#">Community Center</a></li>
+                    <li><a href="#">Ospitare ospiti</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 text-center">
+                  <p class="text-muted">© 2024 Airbnb, Inc.</p>
+                </div>
+              </div>
+            </div>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+.footer {
+    background-color: #f2f2f2;
+    padding: 50px 0;
+  }
+  
+  .footer h5 {
+    font-size: 16px;
+    color: #333;
+  }
+  
+  .footer ul {
+    list-style: none;
+    padding-left: 0;
+  }
+  
+  .footer ul li {
+    margin-bottom: 10px;
+  }
+  
+  .footer ul li a {
+    color: #666;
+  }
+  
+  .footer ul li a:hover {
+    color: #333;
+  }
+    
+</style>
